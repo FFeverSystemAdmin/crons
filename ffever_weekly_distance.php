f7d158cc498e098260b4b3eafa8466a8e09e7bd3
FFeverSystemAdmin <system-admin@ffever.in>
2018-11-10 18:19:11 +0530

    modified

--- ffever_weekly_distance.php
+++ ffever_weekly_distance.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
     $base = __DIR__."/" ;
     include realpath($base."Service/deviceService.php");
     include  realpath($base."Service/cronService.php");
